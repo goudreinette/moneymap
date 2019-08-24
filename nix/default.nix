86c7922ba11ea7bdf42be8d5ad4005bf176b62dd
Michael Bock <me@thought2.de>
2019-08-24 21:35:08 +0200

    Niv

--- nix/default.nix
+++ nix/default.nix
@@ -1,7 +1,19 @@
-{ pkgs ? import <nixpkgs> {}
+{
+  nixpkgs ? import <nixpkgs> {}
+, compiler ? "default"
+, doBenchmark ? false
+, sources ? import ./sources.nix
 }:
 
 let
+  niv = import sources.nixpkgs {
+    overlays = [
+      (_ : _ : { niv = import sources.niv {}; })
+    ] ;
+    config = {};
+  };
+  inherit (niv) pkgs;
+
   nodePkg = (
       import ./node2nix/default.nix { inherit pkgs; }
     ).package;
