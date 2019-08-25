03b36ceadfe14539d1e5663552a7937fc025b13e
Michael Bock <me@thought2.de>
2019-08-25 10:23:01 +0200

    WIP

--- nix/simple.nix
+++ nix/simple.nix
@@ -28,9 +28,6 @@ in pkgs.stdenv.mkDerivation rec {
 
 
   patchPhase = ''
-    # Link `node_modules`
-    rm -rf node_modules
-    ln -sf ${nodePkg}/lib/node_modules/${name}/node_modules .
 
     echo 1
   '';
