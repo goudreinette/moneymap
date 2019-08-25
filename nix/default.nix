6328a5b021e671a02fcff976f30e5436a2dc688a
Michael Bock <me@thought2.de>
2019-08-25 06:46:43 +0200

    WIP

--- nix/default.nix
+++ nix/default.nix
@@ -5,15 +5,16 @@ let
     ).package;
 
   # TODO: Remove, once they're on nixos stable channel
-  # elmTools = import (pkgs.fetchFromGitHub {
-  #   owner = "turboMaCk";
-  #   repo = "nix-elm-tools";
-  #   rev = "41b5045587f84d993a7ee55972cfd61152cafc48";
-  #   sha256 = "1ns02xxj3zijf6myaxk8azgs8v69gpc2b0v080m2xjf1pvv6hd75";
-  # }) { inherit pkgs; };
+  elmTools = import (pkgs.fetchFromGitHub {
+    owner = "turboMaCk";
+    repo = "nix-elm-tools";
+    rev = "41b5045587f84d993a7ee55972cfd61152cafc48";
+    sha256 = "1ns02xxj3zijf6myaxk8azgs8v69gpc2b0v080m2xjf1pvv6hd75";
+  }) { inherit pkgs; };
 
 in pkgs.stdenv.mkDerivation rec {
   name = "moneymap";
+
   src = pkgs.lib.cleanSource ./..;
 
   doCheck = true;
@@ -21,8 +22,8 @@ in pkgs.stdenv.mkDerivation rec {
   buildInputs = with pkgs.elmPackages; [
     elm
     elm-format
-    # elmTools.elm-verify-examples
-    # elmTools.elm-test
+    elmTools.elm-verify-examples
+    elmTools.elm-test
   ];
 
   patchPhase = ''
